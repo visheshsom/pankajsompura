a1690b4a2b3fb49148fe566ed8310525813bff29
visheshsom <visheshsom@gmail.com>
2025-02-25 21:12:37 +0530

    fix1

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [react()], // Ensure the React plugin is added
   build: {
     outDir: "dist", // Make sure this is set correctly
-
-  },
-  optimizeDeps: {
-    exclude: ["react", "react-dom"]
   }
 });
