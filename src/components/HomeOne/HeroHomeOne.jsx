15187123053376a8ee499a73f843364fcb210da2
visheshsom <visheshsom@gmail.com>
2025-02-26 14:40:58 +0530

    slider fix

--- src/components/HomeOne/HeroHomeOne.jsx
+++ src/components/HomeOne/HeroHomeOne.jsx
@@ -1,74 +1,65 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React, { useRef, useEffect } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Autoplay, Navigation, Pagination } from 'swiper/modules';
+import { Autoplay, Navigation } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/navigation';
-import 'swiper/css/pagination';
 import BannerImage1 from '../../assets/images/banner/c3.jpg';
 import BannerImage2 from '../../assets/images/banner/c4.jpg';
 import BannerImage3 from '../../assets/images/banner/c5.jpg';
-import BannerShapeImage1 from '../../assets/images/shape/banner-one-shape.png';
-
-const swiperOptions = {
-    modules: [Autoplay, Pagination, Navigation],
-    slidesPerView: 1,
-    autoplay: {
-        delay: 2000,
-        disableOnInteraction: false,
-    },
-    navigation: true,
-    loop: true,
-};
 
 function HeroHomeOne({ className }) {
-return (
-<>
-    <section className={`banner-area ${className || ''}`}>
-        <div className="container">
-            <div className="banner__image">
+    const prevRef = useRef(null);
+    const nextRef = useRef(null);
 
-                <div className="swiper banner__slider">
-                    <Swiper {...swiperOptions}
-                    modules={[Navigation]}
-                    navigation={{
-                    prevEl: ".banner__arry-prev",
-                    nextEl: ".banner__arry-next",
-                    }}
-                    className="swiper-wrapper">
-                        <SwiperSlide className="swiper-slide">
-                            <div className="parallax-bg"> <img src={BannerImage1} alt="image" data-swiper-parallax="300"/>
-                            </div>
-                        </SwiperSlide>
-                        <SwiperSlide className="swiper-slide">
-                            <div className="parallax-bg"> <img src={BannerImage2} alt="image" data-swiper-parallax="300"/>
-                            </div>
-                        </SwiperSlide>
-                        <SwiperSlide className="swiper-slide">
-                            <div className="parallax-bg"> <img src={BannerImage3} alt="image" data-swiper-parallax="300"/>
-                            </div>
-                        </SwiperSlide>
-                    </Swiper>
-                </div>
+    return (
+        <>
+            <section className={`banner-area ${className || ''}`}>
+                <div className="container">
+                    <div className="banner__image">
+                        {/* Custom Navigation Buttons */}
+                        <button ref={prevRef} className="banner__arry-prev">❮</button>
+                        <button ref={nextRef} className="banner__arry-next">❯</button>
 
-                <div className="banner__content animated-content">
-                    <div className="shape">
-                        <img src={BannerShapeImage1} alt="shape"/>
+                        {/* Swiper Container */}
+                        <div className="banner__slider">
+                            <Swiper
+                                modules={[Autoplay, Navigation]}
+                                slidesPerView={1}
+                                autoplay={{
+                                    delay: 2000,
+                                    disableOnInteraction: false,
+                                }}
+                                loop={true}
+                                onInit={(swiper) => {
+                                    swiper.params.navigation.prevEl = prevRef.current;
+                                    swiper.params.navigation.nextEl = nextRef.current;
+                                    swiper.navigation.init();
+                                    swiper.navigation.update();
+                                }}
+                            >
+                                {/* SwiperSlides */}
+                                <SwiperSlide>
+                                    <div className="parallax-bg">
+                                        <img src={BannerImage1} alt="Banner 1" />
+                                    </div>
+                                </SwiperSlide>
+                                <SwiperSlide>
+                                    <div className="parallax-bg">
+                                        <img src={BannerImage2} alt="Banner 2" />
+                                    </div>
+                                </SwiperSlide>
+                                <SwiperSlide>
+                                    <div className="parallax-bg">
+                                        <img src={BannerImage3} alt="Banner 3" />
+                                    </div>
+                                </SwiperSlide>
+                            </Swiper>
+                        </div>
                     </div>
-                    <h4 className="mb-2" data-animation="fadeInLeft" data-duration="1s" data-delay=".1s">A SPIRIT OF REVERENCE</h4>
-                    <h1 data-animation="fadeInLeft" data-duration="1.5s" data-delay=".15s">Planning, Design, Consultancy, Renovation </h1>
-                    <Link to="/page-about" className="btn-one mt-30" data-animation="fadeInLeft" data-duration="2s" data-delay=".2s">read more</Link>
                 </div>
-
-                <div className="banner__slider-arry">
-                    <button className="banner__arry-prev"><i className="fa-light fa-arrow-left-long"></i></button>
-                    <button className="banner__arry-next"><i className="fa-light fa-arrow-right-long"></i></button>
-                </div>
-            </div>
-        </div>
-    </section>
-</>
-);
+            </section>
+        </>
+    );
 }
 
 export default HeroHomeOne;
